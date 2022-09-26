cebed0d99c2317fdffcc6a4a4f3f79f40c157e3d
MUKESH KUMAR <66511917+Mukesh16407@users.noreply.github.com>
2022-09-26 18:37:39 +0530

    student-model-route

---
+++ model/studentModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const studentSchema = mongoose.Schema({
+    firstName:{
+        type:String,
+        require:true
+    },
+    lastName:{
+        type:String,
+        require:true
+    },
+    rollNo: {
+        type: Number,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+      },
+      phoneNumber: {
+        type: String,
+        required: true,
+      },
+      class: {
+        type: Number,
+        required: true,
+      },
+      results: {
+        type: Array,
+        required: true,
+      },
+});
+
+module.exports = mongoose.model("students", studentSchema);
---
+++ route/studentRoute.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const authMiddleware = require("../middleWares/authMiddleware");
+const router = express.Router();
+const Student = require("../model/studentModel");
+
+// add new student
+router.post('/add-student',authMiddleware,async(req,res)=>{
+
+    try{
+        const studentExists = await Student.findOne({
+            rollNo: req.body.rollNo,
+        });
+        if (studentExists) {
+            return res.status(200).send({
+              message: "Student already exists",
+              success: false,
+            });
+          }
+          const newStudent = new Student(req.body);
+          await newStudent.save();
+          res.status(200).send({
+            message: "Student added successfully",
+            success: true,
+          });
+
+    }catch(error){
+        res.status(500).send({
+            message: error.message,
+            success: false,
+          });
+
+    }
+})
