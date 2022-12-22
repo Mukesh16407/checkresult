8b25d0ff2a495ff07ea10a9de3eb79397335f4f4
Mukesh16407 <mukesh.16407@gmail.com>
2022-12-22 20:43:01 +0530

    deploy on cyclic

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 const connect = require('./config/dbConfig');
 require('dotenv').config();
-const path = require("path");
+
 
 app.use(express.json());
 const employeeRoute = require("./route/employeeRoute");
@@ -16,12 +16,7 @@ app.use("/api/results/", resultsRoute);
 
 
 const port = process.env.PORT || 5000;
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-    });
-  }
+
  
 
 app.listen(port, async()=>{
